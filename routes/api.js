68cf9c38122beac5039c9e6a6ef0757134235dda
Wafa Mohessin <wafamohessin@gmail.com>
2021-12-20 20:41:25 -0500

    still working on it

--- routes/api.js
+++ routes/api.js
@@ -2,24 +2,32 @@ const router = require('express').Router();
 const db = require('../db')
 
 
-/* router.get ('/notes', (req, res) => {
-    db.readNotes ('../db/db.json', (err, data) => {
-        if (err) throw err;
-        res.send(JSON.parse(data));  
-    })
-});  */
     router.get ('/notes', (req, res) => {
     db.readNotes('../db/db.json', 'utf-8', (err,data) => {
         if (err) throw err
         console.log(data)
+        res.send(JSON.parse(data));
         
-        const parsedNotes = JSON.parse(data)
-        parsedNotes.push(newNote)
-        let stringifyNotes = JSON.stringify(parsedNotes)
-        db.writeNotes('../db/db.json', stringifyNotes, (err) => {
-            if (err) throw err
-            res.status(200).send('Note has been saved')
-        })
+        router.post("/notes", (req, res) => {
+           
+            const newNote = { ...req.body, id: uuidv4() };
+            console.log(newNote);
+            console.log(req.body);
+            data.unshift(newNote);
+           
+            fs.writeFile(
+              path.join(__dirname, "../db/db.json"),
+              JSON.stringify(data),
+              function (err) {
+                if (err) {
+                  res.status(404).json({ error: err });
+                }
+                res.json(data);
+              }
+            );
+          });
+          
+
     })})
 
 
