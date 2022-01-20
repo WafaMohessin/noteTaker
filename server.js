eafe436ad8e68254ee9db108d75ad879afa15bb6
Wafa Mohessin <wafamohessin@gmail.com>
2022-01-19 21:19:12 -0500

    fs function in server.js

--- server.js
+++ server.js
@@ -1,20 +1,67 @@
 const express = require('express');
-const apiRoutes = require('./routes/api');
-const htmlRoutes = require('./routes/html');
-//console.log(express)
-
-const app = express ()
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid');
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-// Sets up the Express app to handle data parsing
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
-app.use('/api', apiRoutes)
-app.use ('/', htmlRoutes)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        res.send(JSON.parse(data));  
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = uuid.v4();
+    const note = req.body;
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const obj = JSON.parse(data);
+        console.log(obj);
+        obj.push(note);
+        fs.writeFile('./db/db.json', JSON.stringify(obj), (err, data) => {
+            if (err) throw err;
+            fs.readFile('./db/db.json', (err, data) => {
+                if (err) throw err;
+                res.send(JSON.parse(data));  
+            });
+        } );
+    });  
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const obj = JSON.parse(data);
+        obj.forEach(element => {
+            if (element.id === req.params.id) {
+                obj.splice(obj.indexOf(element));
+            }
+        });
+        
+        fs.writeFile('./db/db.json', JSON.stringify(obj), (err, data) => {
+            if (err) throw err;
+                if (err) throw err;
+                res.status(200).json(`Note deleted successfully!`); 
+        } );
+    });  
+});
 
 app.listen(PORT, () => {
-    console.log(`Note Taker listening on http://localhost:${PORT}`)
-})
+    console.log(`Listening on port ${PORT}`);
+});
